2fd96c9e4cdaaef554cfbb91bd5121232f3471d9
Denis Solodovnik <solodovnik@improveitgroup.com>
2021-07-29 22:31:24 +0700

    Initial Commit

---
+++ XIAGTest/ViewController.swift
@@ -0,0 +1,17 @@
+//
+// XIAGTest
+// 
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
